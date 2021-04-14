6169beba63ffc02f7a2396100611607e02be3438
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2021-04-14 09:22:09 +0100

    Initial Commit

---
+++ SuperWASPSLSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SuperWASPSLSim
+//
+//  Created by Hugh Dickinson on 14/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
