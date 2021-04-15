a284f93da1c2b3bb0fbe26b1b10d018d56680e31
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2021-04-15 14:30:19 +0100

    Initial commit

---
+++ SuperWASPSLSim/include/EinsteinRadius.hpp
@@ -0,0 +1,56 @@
+//
+//  EinsteinRadius.h
+//  SuperWASPSLSim
+//
+//  Created by Hugh Dickinson on 15/04/2021.
+//
+
+#ifndef EinsteinRadius_h
+#define EinsteinRadius_h
+
+#include <boost/units/base_units/astronomical/parsec.hpp>
+#include <boost/units/static_rational.hpp>
+#include <boost/units/make_scaled_unit.hpp>
+#include <boost/units/systems/si/codata_constants.hpp>
+#include <boost/units/systems/si/length.hpp>
+#include <boost/units/systems/si/mass.hpp>
+#include <boost/units/cmath.hpp>
+#include <boost/units/io.hpp>
+
+using namespace boost::units;
+
+typedef unit<length_dimension, make_system<astronomical::parsec_base_unit>::type> parsec_unit;
+BOOST_UNITS_STATIC_CONSTANT(parsec,parsec_unit);
+
+typedef make_scaled_unit<parsec_unit, scale<10, static_rational<3> > >::type kiloparsec_unit;
+BOOST_UNITS_STATIC_CONSTANT(kiloparsec,kiloparsec_unit);
+
+template <typename EinsteinRadiusUnit, typename SourceToObsUnit, typename LensToObsUnit, typename SourceToLensUnit, typename CompactObjectMassUnit>
+auto sourcePlaneEinsteinRadius(
+                               quantity<SourceToObsUnit> const & sourceToObsDist,
+                               quantity<LensToObsUnit> const & lensToObsDist,
+                               quantity<SourceToLensUnit> const & sourceToLensDist,
+                               quantity<CompactObjectMassUnit> const & lensMass){
+    auto G = boost::units::si::constants::codata::G;
+    auto C = boost::units::si::constants::codata::c;
+    auto einsteinRadius = boost::units::sqrt((4.0 * G * lensMass * sourceToLensDist * lensToObsDist)
+                                             / (sourceToObsDist * C * C));
+    
+    return std::unique_ptr<quantity<EinsteinRadiusUnit> >(new quantity< EinsteinRadiusUnit>(einsteinRadius));
+    
+}
+
+template <typename EinsteinRadiusUnit, typename SourceToObsUnit, typename LensToObsUnit, typename SourceToLensUnit, typename CompactObjectMassUnit>
+auto lensPlaneEinsteinRadius(
+                             quantity<SourceToObsUnit> const & sourceToObsDist,
+                             quantity<LensToObsUnit> const & lensToObsDist,
+                             quantity<SourceToLensUnit> const & sourceToLensDist,
+                             quantity<CompactObjectMassUnit> const & lensMass){
+    
+    auto einsteinRadius = (sourceToObsDist / lensToObsDist) * *sourcePlaneEinsteinRadius<EinsteinRadiusUnit>(sourceToObsDist,lensToObsDist, sourceToLensDist, lensMass);
+    
+    return std::unique_ptr<quantity<EinsteinRadiusUnit> >(new quantity< EinsteinRadiusUnit>(einsteinRadius));
+    
+}
+
+#endif /* EinsteinRadius_h */
--- SuperWASPSLSim/main.cpp
+++ SuperWASPSLSim/main.cpp
@@ -6,9 +6,51 @@
 //
 
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <array>
+#include <algorithm>
+#include <iterator>
+
+#include "EinsteinRadius.hpp"
+#include "NonUniformCircularSource.h"
+
+template<int NumSteps>
+auto magnifications(double radiusUnitless, std::vector<double> range = {-1, 1}){
+    
+    auto magnifier  = NonUniformCircularSource(radiusUnitless);
+    
+    auto impactParam = 0.05;
+    auto magnifications = std::array<double, NumSteps>{};
+    
+    auto elementCounter = 0;
+    std::generate(magnifications.begin(), magnifications.end(), [&](){
+        auto x = range[0] + elementCounter * (range[1] - range[0])/static_cast<float>(NumSteps);
+        auto offset = std::hypot(x, impactParam);
+        elementCounter++;
+        return magnifier.magnification(offset);
+    });
+    
+    return magnifications;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    auto solarRadius = 6.957e8*boost::units::si::meters;
+    auto astronomicalUnit = 1.495978707e11*boost::units::si::meters;
+    auto solarMass = 1.988409870698051e30*boost::units::si::kilograms;
+    
+    auto systemDistance = quantity<si::length>(2 * kiloparsec);
+    auto systemSeparation = 1000.0*astronomicalUnit;
+    auto lensMass = 20.0 * solarMass;
+    auto sourceRadius = 5.0 * solarRadius;
+    
+    auto radiusUnitless = quantity<si::length>(sourceRadius) / *sourcePlaneEinsteinRadius<si::length>(systemDistance, systemDistance - systemSeparation, systemSeparation, lensMass);
+    std::cout << radiusUnitless << "\t" << quantity<si::length>(sourceRadius)/radiusUnitless << "\t" << quantity<si::length>(sourceRadius) << std::endl;
+    auto magValues = magnifications<200>(radiusUnitless);
+    std::copy(magValues.begin(), magValues.end(), std::ostream_iterator<double>(std::cout, ", "));
     return 0;
 }
